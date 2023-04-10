3a98af097e0fcec99807b6ebb7ecb5aa42f47f42
MarshallLiu-Dev <paulomartins12358@gmail.com>
2023-04-10 12:39:39 -0300

    first commit

---
+++ src/aulaencap/Soma.java
@@ -0,0 +1,16 @@
+package aulaencap;
+
+public class Soma {
+
+//Atributos Variaveis	
+	float Valor1;
+	float Valor2;
+	float resposta = 0;
+//Função
+	
+	float calcular() {
+		resposta = Valor1 + Valor2;
+		return resposta;	
+	}
+	
+}
---
+++ src/aulaencap/principal.java
@@ -0,0 +1,14 @@
+package aulaencap;
+
+public class principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// declarar uma instancia de objeto
+		//class obj = new Construtor()
+		
+		Soma soma1 = new Soma();
+		soma1.Valor1 = (float)5.2;
+		System.out.println(soma1.Valor1);
+	}		
+}
