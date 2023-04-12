590ff5c44516c24ea9cb8339575ff33a434ce795
Paulo Henrique M de Morais <111690624+MarshallLiu-Dev@users.noreply.github.com>
2023-04-12 12:32:56 -0300

    first commit

--- src/aulaencap/Soma.java
+++ src/aulaencap/Soma.java
@@ -3,9 +3,9 @@ package aulaencap;
 public class Soma {
 
 //Atributos Variaveis	
-	float Valor1;
-	float Valor2;
-	float resposta = 0;
+private	float Valor1;
+private	float Valor2;
+private	float resposta = 0;
 //Função
 	
 	float calcular() {
@@ -13,4 +13,19 @@ public class Soma {
 		return resposta;	
 	}
 	
+	public void setValor1(float v){
+		Valor1 = v;
+	}
+
+	public void setValor2(float v){
+		Valor2 = v;
+	}
+
+	public float getValor1(){
+		return Valor1;
+	}
+	public float getValor2()[
+		return Valor2;
+	]
+
 }
--- src/aulaencap/principal.java
+++ src/aulaencap/principal.java
@@ -11,4 +11,22 @@ public class principal {
 		soma1.Valor1 = (float)5.2;
 		System.out.println(soma1.Valor1);
 	}		
+
+	float v1,
+	float v2,
+	float fb
+
+	Soma soma1 = new Soma();
+
+	System.out.println("Digite o 1ª primeiro valor ")
+	v1 = Pix.nextFloat();
+	System.out.println("Digite o 2ª primeiro valor ")
+	v2 = Pix.nextFloat();
+
+	soma1.setValor1(v1);
+	soma1.setValor2(v2);
+
+	fg = soma1.calcular();
+
+	System.out.println(fg);
 }
